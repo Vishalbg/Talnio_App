707788523a179e43a25d533bd2e285c2c7607797
vishalbg02 <vishalbg02@gmail.com>
2025-07-03 16:27:11 +0530

    mac_Lib_Theme_update

--- lib/theme.dart
+++ lib/theme.dart
@@ -14,7 +14,7 @@ ThemeData appTheme() {
     textTheme: GoogleFonts.robotoTextTheme(),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
-        padding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
         textStyle: GoogleFonts.roboto(fontSize: 16, fontWeight: FontWeight.w500),
       ),
@@ -22,19 +22,20 @@ ThemeData appTheme() {
     inputDecorationTheme: InputDecorationTheme(
       border: OutlineInputBorder(
         borderRadius: BorderRadius.circular(12),
-        borderSide: BorderSide(color: Colors.grey),
+        borderSide: const BorderSide(color: Colors.grey),
       ),
       filled: true,
       fillColor: Colors.grey[100],
-      contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+      contentPadding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
       labelStyle: GoogleFonts.roboto(color: Colors.grey[600]),
       errorStyle: GoogleFonts.roboto(color: Colors.redAccent),
     ),
-    cardTheme: CardTheme(
+    cardTheme: const CardThemeData(
       elevation: 4,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(12)),
+      ),
       margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
     ),
   );
 }
-
